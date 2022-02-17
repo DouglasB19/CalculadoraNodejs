e87c9c050c16cf743769d61439bcdd25da0075d9
DouglasB19 <55929934+DouglasB19@users.noreply.github.com>
2022-02-17 19:13:07 -0300

    third commit

--- app.js
+++ app.js
@@ -2,7 +2,9 @@
 const express=require('express');
 const app=express();
 const bodyparser=require('body-parser');
+const funcao=require('./components/functions');
 
+let funcaos=new funcao();
 let port=process.env.PORT || 3000;
 
 app.use(bodyparser.urlencoded({extended: true}));
@@ -10,15 +12,8 @@ app.use(bodyparser.urlencoded({extended: true}));
 app.use('/css',express.static('css'));
 app.use('/js',express.static('js'));
 
-app.get('/', (req, res)=>{res.sendFile(__dirname+"/index.html")});
+app.get('/', function(req, res){res.sendFile(__dirname+"/index.html")});
 
-app.post('/', (req, res)=>{
-    const n1 = Number(req.body.number1);
-    const n2 = Number(req.body.number1);
-
-    const soma = n1 + n2;
-
-    res.send("Resultado da soma é : "+ soma);
-})
+app.post('/', function(req, res){funcaos.somar(req,res)});
 
 app.listen(port);
--- components/functions.js
+++ components/functions.js
@@ -1,31 +1,35 @@
-class Functions{
+class Funcao{
 
     somar(req,res){
-        const n1 = req.body.number1;
-        const n2 = req.body.number1;
+        const n1 = Number(req.body.number1);
+        const n2 = Number(req.body.number2);
     
         const soma = n1 + n2;
+        res.send(soma.toString());
     }
 
     subtrair(req,res){
-        const n1 = req.body.number1;
-        const n2 = req.body.number1;
+        const n1 = Number(req.body.number1);
+        const n2 = Number(req.body.number2);
     
-        const soma = n1 - n2;
+        const subtrai = n1 - n2;
+        res.render(subtrai);
     }
 
     multiplicar(req,res){
-        const n1 = req.body.number1;
-        const n2 = req.body.number1;
+        const n1 = Number(req.body.number1);
+        const n2 = Number(req.body.number2);
     
-        const soma = n1 * n2;
+        const multiplica = n1 * n2;
+        res.render(multiplica);
     }
 
     dividir(req,res){
-        const n1 = req.body.number1;
-        const n2 = req.body.number1;
+        const n1 = Number(req.body.number1);
+        const n2 = Number(req.body.number2);
     
-        const soma = n1 / n2;
+        const divide = n1 / n2;
+        res.render(divide);
     }
 }
-module.exports=Functions;
+module.exports=Funcao;
